4d06319e7d800c08dff70bc9240a9e7064f9be0e
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-09-11 22:04:09 +0100

    petit modification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import {SignupComponent} from "./signup/signup.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminComponent} from "./admin/admin.component";
-import {HomeComponent} from "./home/home.component";
 import {UserDashbordComponent} from "./user-dashbord/user-dashbord.component";
 
 export const routes: Routes = [
